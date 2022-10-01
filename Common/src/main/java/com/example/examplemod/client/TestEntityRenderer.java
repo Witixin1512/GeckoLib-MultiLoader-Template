3189e8e22a9626f4e9efa6c93a66fc8b76ba9d8e
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2022-10-01 17:54:46 +0200

    Revert "Update loom to 1.+, add parchment to forge and fabric"
    This reverts commit 9bba4192daa36b98cf7a5871fbe59354663e3d25.

--- Common/src/main/java/com/example/examplemod/client/TestEntityRenderer.java
+++ Common/src/main/java/com/example/examplemod/client/TestEntityRenderer.java
@@ -2,6 +2,7 @@ package com.example.examplemod.client;
 
 import com.example.examplemod.TestEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
 public class TestEntityRenderer extends GeoEntityRenderer<TestEntity> {
